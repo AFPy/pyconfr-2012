82c478d80b34649b72c727a9f3a152cc715ea5a2
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-04-28 22:41:54 +0200

    Migrated from hg to git

---
+++ src/pyconfr/settings_test.py
@@ -0,0 +1,12 @@
+
+DATABASE_ENGINE = 'sqlite3'
+
+DATABASE_NAME = '/tmp/pyconfr.db'
+
+INSTALLED_APPS = [
+    'pyconfr'
+    ]
+
+ROOT_URLCONF = [
+    'pyconfr.urls'
+    ]
---
+++ src/pyconfr/tests.py
@@ -0,0 +1,8 @@
+# django import
+from django.test import TestCase
+
+
+class ShortURLTests(TestCase):
+
+    def test_environment(self):
+        self.assert_(True)
