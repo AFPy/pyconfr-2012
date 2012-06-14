777cc6352eaf4770104aba812f572ad7863ab483
florent <florent@toopy.org>
2012-06-15 00:57:17 +0200

    ensure media serving

--- src/pyconfr/settings.py
+++ src/pyconfr/settings.py
@@ -69,7 +69,7 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/media/"
+MEDIA_URL = "/%s/media/" % PYCON_YEAR
 
 # Absolute path to the directory that holds static files like app media.
 # Example: "/home/media/media.lawrence.com/apps/"
